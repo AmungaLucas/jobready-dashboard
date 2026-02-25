ba152b6901e7092e9c494877c28a8e5228250a24
Amunga Lucas <amungalucas@gmail.com>
2026-02-25 22:46:54 +0300

    api error fix

--- proxy.js
+++ proxy.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { adminAuth } from './src/lib/firebaseAdmin';
+import { adminAuth } from '@/lib/firebaseAdmin';
 
 export async function proxy(request) {
   const session = request.cookies.get('session')?.value || '';
